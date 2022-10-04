79ec57f08bd778b2eced6aca01b1dc8a3eb909c1
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-04 16:08:02 -0600

    fixed file import

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 // configuration for dotenv
-require("dotenv").config({ path: "config.env" });
+require("dotenv").config({ path: "./config.env" });
 // declaring the port to be used for the server
 const port = process.env.PORT || 3000;
 // express middleware
