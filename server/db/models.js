cf7673aabca90630ac73c90c097da41f0a764842
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-07 15:58:37 -0600

    commented out models code

---
+++ server/db/models.js
@@ -0,0 +1,8 @@
+// let db = require("../db/connection");
+
+// db.collection("reviews").insertOne({
+//   sellerName: "Candace",
+//   productName: "Mean Green E-Scooter",
+//   review: "This scooter is amazing, I love it!",
+//   tag: ["seller review"],
+// });
