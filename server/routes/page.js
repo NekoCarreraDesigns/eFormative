d2f20836b2d592f6c2eee4c7f790528fd585057b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-07 15:59:44 -0600

    added routes for product reviews

--- server/routes/page.js
+++ server/routes/page.js
@@ -4,6 +4,7 @@ const express = require("express");
 const pageRoutes = express.Router();
 // database connection
 const db = require("../db/connection");
+const models = require("../db/models");
 // convert string to object
 const ObjectId = require("mongodb").ObjectId;
 // routes for users with queries to database
@@ -45,7 +46,7 @@ pageRoutes.route("/user/update/:id").post(function (req, response) {
   let db_connect = db.getDb();
   let addQuery = { _id: ObjectId(req.params.id) };
   let newValues = {
-    set: {
+    $set: {
       name: req.name.body,
       username: req.username.body,
     },
@@ -67,6 +68,82 @@ pageRoutes.route("/user/:id").delete((req, response) => {
     console.log("User deleted!");
     response.json(obj);
   });
+});
+
+// routes for reviews of sellers and product
+pageRoutes.route("/reviews").get(function (req, res) {
+  let db_connect = db.getDb();
+  db_connect
+    .collection("reviews")
+    .find({})
+    .toArray(function (req, result) {
+      if (err) res.status(404);
+      res.json(result);
+    });
+});
+
+pageRoutes.route("/seller/reviews/:id").get(function (req, res) {
+  let db_connect = db.getDb();
+  let sellerQuery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("reviews").findOne(sellerQuery, function (req, result) {
+    if (err) res.status(404);
+    res.json(result);
+  });
+});
+
+pageRoutes.route("/product/reviews/:id").get(function (req, res) {
+  let db_connect = db.getDb();
+  let productQuery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("reviews")
+    .findOne(productQuery, function (req, result) {
+      if (err) res.status(404);
+      res.json(result);
+    });
+});
+
+pageRoutes.route("/product/reviews/post").post(function (req, res) {
+  let db_connect = db.getDb();
+  let postObj = {
+    sellerName: req.body.sellerName,
+    productName: req.body.productName,
+    review: req.body.review,
+  };
+  db_connect.collection("reviews").insertOne(postObj, function (err, result) {
+    if (err) res.status(404);
+    res.json(result);
+  });
+});
+
+pageRoutes.route("/reviews/update/:id").post(function (req, response) {
+  let db_connect = db.getDb();
+  let updatePostQuery = { _id: ObjectId(req.params.id) };
+  let newPostValues = {
+    $set: {
+      sellerName: req.sellerName.body,
+      productName: req.productName.body,
+      review: req.review.body,
+    },
+  };
+  db_connect
+    .collection("reviews")
+    .updateOne(updatePostQuery, newPostValues, function (err, res) {
+      if (err) res.status(404);
+      console.log("Update Successful!");
+      response.json();
+    });
+});
+
+pageRoutes.route("/reviews/:id").delete((req, response) => {
+  let db_connect = db.getDb();
+  let deleteReviewQuery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("reviews")
+    .deleteOne(deleteReviewQuery, function (err, obj) {
+      if (err) res.status(404);
+      console.log("Review deleted!");
+      response.json(obj);
+    });
 });
 
 module.exports = pageRoutes;
