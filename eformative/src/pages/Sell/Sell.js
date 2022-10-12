fbbecb8c911478a889c4c283984b3b2649e3fbf1
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-12 17:50:03 -0600

    created basic sell page layout

--- eformative/src/pages/Sell/Sell.js
+++ eformative/src/pages/Sell/Sell.js
@@ -2,7 +2,36 @@ import React from "react";
 import "./Sell.css";
 
 const Sell = () => {
-  return <h1 className='sell-page-header'>Sell</h1>;
+  return (
+    <>
+      <h1 className='sell-page-header'>Sell</h1>
+      <p className='sell-page-paragraph'>
+        Please login if you are a returning seller,
+        <br /> or sign up to become a new seller
+      </p>
+      <div className='sell-form-div'>
+        <form className='sell-page-form'>
+          <input
+            className='username-input'
+            type='text'
+            placeholder='please type username'
+            name='username'></input>
+          <br />
+          <input
+            className='user-password-input'
+            type='password'
+            placeholder='please enter password'
+            name='user-password'></input>
+          <br />
+          <button className='user-login-submit-button' type='submit'>
+            Login
+          </button>
+        </form>
+      </div>
+      <p className='new-seller-paragraph'>New sellers please sign up here</p>
+      <button className='user-signup-redirect'>signup</button>
+    </>
+  );
 };
 
 export default Sell;
