8263dbcdffdbf216098e4973f8fa308ea4ccc835
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-13 15:12:41 -0600

    created signup form with redirect function to post item

---
+++ eformative/src/pages/Signup/Signup.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Signup.css";
+
+const Signup = () => {
+  let navigate = useNavigate();
+  const postRedirect = () => {
+    let path = `/post-item`;
+    navigate(path);
+  };
+  return (
+    <>
+      <h1 className='signup-page-header'>Please Sign Up</h1>
+      <div className='signup-form-container'>
+        <form className='signup-form'>
+          <input
+            className='fullName-input'
+            placeholder='please enter full name'
+            name='fullName'></input>
+          <br />
+          <input
+            className='userName-input'
+            placeholder='please enter a username'
+            name='userName'></input>
+          <br />
+          <input
+            className='password-input'
+            placeholder='please enter a password'
+            name='password0'></input>
+          <br />
+          <input
+            className='password-re-enter-input'
+            placeholder='please re-enter password'
+            name='password1'></input>
+          <br />
+          <button className='user-signup-button' onClick={postRedirect}>
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
