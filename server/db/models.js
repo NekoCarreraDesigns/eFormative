5c575ba30d62b861d71ee47816115f5af62c5a2e
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-25 15:35:38 -0600

    removed old code

--- server/db/models.js
+++
@@ -1,8 +0,0 @@
-// let db = require("../db/connection");
-
-// db.collection("reviews").insertOne({
-//   sellerName: "Candace",
-//   productName: "Mean Green E-Scooter",
-//   review: "This scooter is amazing, I love it!",
-//   tag: ["seller review"],
-// });
