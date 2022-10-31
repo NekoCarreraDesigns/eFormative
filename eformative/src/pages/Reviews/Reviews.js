283821b7d79c4323230ce11ebf0cdf32f25536e9
NekoCarreraDesigns <denvermaas@gmail.com>
2022-10-31 15:55:58 -0600

    created map function to display the reviews from the API, created hook to display the reviews

--- eformative/src/pages/Reviews/Reviews.js
+++ eformative/src/pages/Reviews/Reviews.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Reviews.css";
 
 const Reviews = () => {
-  return <h1 className='reviews-page-header'>Reviews</h1>;
+  const [reviews, setReview] = useState();
+
+  useEffect(() => {
+    fetch("/reviews")
+      .then((res) => res.json())
+      .then((reviews) => setReview(reviews));
+  });
+
+  return (
+    <>
+      <h1 className='reviews-page-header'>Reviews</h1>
+      {reviews?.map((review, userReview) => (
+        <div key={userReview}>
+          <h1 className='reviews-header'>
+            Seller: {review.sellerName}, Product: {review.productName}
+          </h1>{" "}
+          <div className='reviews-div'>{review.review}</div> <hr />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Reviews;
