117afc5e8114d211566c3bf55cdbd37d957269f2
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-01 16:37:11 -0600

    added seller filter function, needs work to filter properly and get proper response

--- eformative/src/pages/Reviews/Reviews.js
+++ eformative/src/pages/Reviews/Reviews.js
@@ -4,6 +4,7 @@ import "./Reviews.css";
 
 const Reviews = () => {
   const [reviews, setReview] = useState();
+  const [seller, setSeller] = useState();
 
   useEffect(() => {
     fetch("/reviews")
@@ -11,9 +12,34 @@ const Reviews = () => {
       .then((reviews) => setReview(reviews));
   });
 
+  const sellerFilter = () => {
+    fetch("/seller/reviews/:id")
+      .then((response) => response.json())
+      .then((seller) => setSeller(seller));
+    console.log(seller);
+  };
+
   return (
     <>
       <h1 className='reviews-page-header'>Reviews</h1>
+      <div className='search-filters-div'>
+        <div className='seller-filter-div'>
+          <h2 className='search-filters-header'>Search filters</h2>
+          <input type='text' placeholder='search by seller name'></input>
+          <button
+            className='search-filters-seller-button'
+            onClick={sellerFilter}>
+            Search Seller
+          </button>
+          <input
+            className='product-filter-input'
+            type='text'
+            placeholder='search by product name'></input>
+          <button className='search-filters-product-button'>
+            Search Product
+          </button>
+        </div>
+      </div>
       {reviews?.map((review, userReview) => (
         <div key={userReview}>
           <h1 className='reviews-header'>
