2535e5d235bd9807ea141f33511f9a9e1a6ffc81
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-07 14:25:58 -0700

    created hook to display items from the database

--- eformative/src/pages/Market/Market.js
+++ eformative/src/pages/Market/Market.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Market.css";
 
 const Market = () => {
-  return <h1 className='market-page-header'>Market</h1>;
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    fetch("/market")
+      .then((res) => res.json())
+      .then((items) => setItems(items));
+  });
+
+  return (
+    <>
+      <h1 className='market-page-header'>Market</h1>;
+      {items?.map((item, sellerItem) => (
+        <div key={sellerItem}>
+          <h1 className='item-header'>
+            {" "}
+            Seller: {item.sellerName}, Product: {item.product}
+          </h1>
+          <div>
+            <h3 className='item-price-header'>{item.price}</h3>
+            <img alt='product' src='http://placehold.jp/150x150.png'></img>
+          </div>
+          <p className='item-description-paragraph'>{item.description}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Market;
