b3edb9e6d01a6099c55d0757c4d03c1e484d2385
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-07 15:01:55 -0700

    edited item card

--- eformative/src/pages/Market/Market.js
+++ eformative/src/pages/Market/Market.js
@@ -15,9 +15,9 @@ const Market = () => {
       <h1 className='market-page-header'>Market</h1>;
       {items?.map((item, sellerItem) => (
         <div className='item-card' key={sellerItem}>
-          <div>
-            <h1 className='item-seller-header'> Seller: {item.sellerName}</h1>
-            <h2 className='item-product-header'>Product: {item.product}</h2>
+          <div className='item'>
+            <h1 className='item-seller-header'>{item.sellerName}</h1>
+            <h2 className='item-product-header'>{item.product}</h2>
             <h3 className='item-price-header'>{item.price}</h3>
             <img alt='product' src='http://placehold.jp/150x150.png'></img>
             <p className='item-description-paragraph'>{item.description}</p>
