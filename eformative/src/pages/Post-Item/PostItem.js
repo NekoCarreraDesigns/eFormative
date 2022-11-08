e56b96a90231b41f67fe3a042e8e7ac8b0109425
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-08 13:29:57 -0700

    created function to post item to database

--- eformative/src/pages/Post-Item/PostItem.js
+++ eformative/src/pages/Post-Item/PostItem.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./PostItem.css";
 
-const username = "SnuggleBunny84";
+const sellerPostInput = document.querySelector("input");
+const productNamePostInput = document.getElementById("item-name");
+const priceInput = document.getElementById("item-price");
+const productDescriptionArea = document.querySelector("textarea");
+
+const addItem = (event) => {
+  axios
+    .post("/items/add", {
+      sellerName: sellerPostInput.value,
+      product: productNamePostInput.value,
+      price: priceInput.value,
+      description: productDescriptionArea.value,
+    })
+    .then((res) => {
+      res.status(200);
+      alert("Your item has been posted!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  event.preventDefault();
+};
 
 const PostItem = () => {
+  const [inputArea, setInputArea] = useState("");
+  const characterCounter = (event) => {
+    setInputArea(event.target.value);
+  };
+
   return (
     <>
-      <h1 className='post-item-header'>Welcome {username}!</h1>;
+      <h1 className='post-item-header'>Welcome!</h1>;
       <p className='post-item-paragraph'>Please post your item!</p>;
       <div className='post-item-form-div'>
         <form>
           <input
+            className='seller-name-post-input'
+            type='text'
+            placeholder='please input your name'></input>
+          <br />
+          <input
             className='item-name-input'
             type='text'
-            name='item-name'
+            id='item-name'
             placeholder='Please input item name'></input>
           <br />
           <button
@@ -25,12 +57,20 @@ const PostItem = () => {
           <input
             className='item-price-input'
             type='text'
-            name='item-price'
+            id='item-price'
             placeholder='Please input a price'></input>
           <br />
-          <textarea className='post-item-textarea' type='text'></textarea>
+          <textarea
+            maxLength={300}
+            id='post-item-area'
+            className='post-item-textarea'
+            onChange={characterCounter}></textarea>
           <br />
-          <button className='add-item-button' type='submit'>
+          <span className='character-count-post-span'>
+            <strong>{300 - inputArea.length} characters left</strong>
+          </span>
+          <br />
+          <button className='add-item-button' onClick={addItem}>
             Post Item
           </button>
         </form>
