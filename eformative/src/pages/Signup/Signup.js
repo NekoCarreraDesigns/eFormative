d8df9a8b5d4ac1f40e60196104e423b265cd712f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-10 16:20:55 -0700

    working on redirect for signup

--- eformative/src/pages/Signup/Signup.js
+++ eformative/src/pages/Signup/Signup.js
@@ -1,41 +1,36 @@
 import React from "react";
 import axios from "axios";
-// import { useNavigate } from "react-router-dom";
 import "./Signup.css";
 
-const fullNameInput = document.querySelector("input");
-const usernameInput = document.getElementById("userName");
-const userEmailInput = document.getElementById("email");
-const passwordInput0 = document.getElementById("password0");
-
-const userPostSignUp = () => {
-  axios
-    .post("/seller", {
-      fullName: fullNameInput.value,
-      username: usernameInput.value,
-      email: userEmailInput.value,
-      password: passwordInput0.value,
-    })
-    .then((res) => {
-      res.redirect("/post-item");
-      console.log("Successful Sign Up!");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
 const Signup = () => {
-  // let navigate = useNavigate();
-  // const postRedirect = () => {
-  //   let path = `/post-item`;
-  //   navigate(path);
-  // };
+  const fullNameInput = document.querySelector("input");
+  const usernameInput = document.getElementById("userName");
+  const userEmailInput = document.getElementById("email");
+  const passwordInput0 = document.getElementById("password0");
+
+  const userPostSignUp = () => {
+    axios
+      .post("/seller", {
+        fullName: fullNameInput.value,
+        username: usernameInput.value,
+        email: userEmailInput.value,
+        password: passwordInput0.value,
+      })
+      .then((res) => {
+        console.log(res);
+        alert("Successful Sign Up");
+        window.location.replace("http://localhost:3000/post-item");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <h1 className='signup-page-header'>Please Sign Up</h1>
       <div className='signup-form-container'>
-        <form className='signup-form'>
+        <form onSubmit={userPostSignUp} className='signup-form'>
           <input
             className='fullName-input'
             placeholder='please enter full name'></input>
@@ -57,7 +52,7 @@ const Signup = () => {
             placeholder='please enter a password'
             id='password0'></input>
           <br />
-          <button className='user-signup-button' onClick={userPostSignUp}>
+          <button className='user-signup-button' type='submit'>
             Sign Up
           </button>
         </form>
