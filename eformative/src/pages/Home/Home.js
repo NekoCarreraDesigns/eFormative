45356818820564343577337a1277646e7dfac8e2
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-11 16:20:14 -0700

    added paragraph and function for users geolocation, with redirect to market

--- eformative/src/pages/Home/Home.js
+++ eformative/src/pages/Home/Home.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <h1 className='home-page-header'>Eformative</h1>;
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position);
+      window.location.replace("/market");
+    });
+  };
+
+  return (
+    <>
+      <h1 className='home-page-header'>Eformative</h1>
+      <p className='home-page-paragraph'>
+        Welcome to Eformative! A place for all your electric vehicle and
+        alternative transportation needs. Check out the market place, look for
+        reviews of products , and sellers. Sell an item of yours. let's help
+        build a greener world for future generations.{" "}
+      </p>
+      <button className='home-location-button' onClick={getLocation}>
+        Find your Market
+      </button>
+    </>
+  );
 };
 
 export default Home;
