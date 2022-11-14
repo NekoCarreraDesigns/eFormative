f2aeeab32bee858800a33e63ea8cc98abae612f6
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-14 13:29:45 -0700

    added function for login

--- eformative/src/pages/Sell/Sell.js
+++ eformative/src/pages/Sell/Sell.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Sell.css";
 
 const Sell = () => {
@@ -14,6 +15,27 @@ const Sell = () => {
     navigate(sellerPath);
   };
 
+  const userSignIn = (event) => {
+    const usernameInput = document.getElementById("username");
+    const userPasswordInput = document.getElementById("user-password");
+
+    event.preventDefault();
+    axios
+      .post("/sign-in", {
+        username: usernameInput.value,
+        password: userPasswordInput.value,
+      })
+      .then((response) => {
+        response.json({ message: "login successful" });
+        sellerRedirect();
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  };
+
   return (
     <>
       <h1 className='sell-page-header'>Sell</h1>
@@ -22,23 +44,20 @@ const Sell = () => {
         <br /> or sign up to become a new seller
       </p>
       <div className='sell-form-div'>
-        <form className='sell-page-form'>
+        <form onSubmit={userSignIn} className='sell-page-form'>
           <input
             className='username-input'
             type='text'
             placeholder='please type username'
-            name='username'></input>
+            id='username'></input>
           <br />
           <input
             className='user-password-input'
             type='password'
             placeholder='please enter password'
-            name='user-password'></input>
+            id='user-password'></input>
           <br />
-          <button
-            className='user-login-submit-button'
-            type='submit'
-            onClick={sellerRedirect}>
+          <button className='user-login-submit-button' type='submit'>
             Login
           </button>
         </form>
