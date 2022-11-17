f4b9d86432772061567f5b3119cc3955c15bfd18
NekoCarreraDesigns <denvermaas@gmail.com>
2022-11-17 15:47:32 -0700

    added code to get current user, currently not working

--- eformative/src/pages/Seller/Seller.js
+++ eformative/src/pages/Seller/Seller.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Seller.css";
 
-let seller = "Candace";
-
 const Seller = () => {
+  const [user, setUser] = useState(null);
   let navigate = useNavigate();
   const postItemRedirect = () => {
     let postItemPath = `/post-item`;
     navigate(postItemPath);
   };
+
+  useEffect(() => {
+    fetch("/user/:id")
+      .then((res) => res.json())
+      .then((user) => setUser(user))
+      .catch((err) => console.log(err));
+  });
+
   return (
     <>
-      <h1 className='seller-page-header'>Welcome {seller}</h1>
+      {user?.map((user, userDisplay) => (
+        <h1 key={userDisplay} className='seller-page-header'>
+          Welcome {user.username}
+        </h1>
+      ))}
       <button className='post-item-button' onClick={postItemRedirect}>
         Post an item to sell
       </button>
