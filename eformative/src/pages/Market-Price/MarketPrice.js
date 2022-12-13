8ac64840ad11237b18e4d4c51acc53c58f4e20e3
NekoCarreraDesigns <denvermaas@gmail.com>
2022-12-13 14:47:51 -0700

    added code to function, with a fetch to an api, and display the information in a list with links

--- eformative/src/pages/Market-Price/MarketPrice.js
+++ eformative/src/pages/Market-Price/MarketPrice.js
@@ -3,16 +3,26 @@ import "./MarketPrice.css";
 
 const MarketPrice = () => {
   const [price, setPrice] = useState();
+
+  useEffect(() => {
+    fetch("/market/products")
+      .then((res) => res.json())
+      .then((price) => setPrice(price));
+  });
+
   return (
     <>
-      <h1>View by price</h1>
+      <h1 className='market-price-header'>View by price</h1>
       {price?.map((price, displayPrice) => (
         <div key={displayPrice}>
           <ul>
             <li>{price.price}</li>
+            <li>{price.productName}</li>
           </ul>
         </div>
       ))}
     </>
   );
 };
+
+export default MarketPrice;
