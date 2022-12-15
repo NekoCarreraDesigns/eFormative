a300c6fce3da82ca46f34f46fedb85caa5594b7a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-12-15 14:20:31 -0700

    added image

--- eformative/src/pages/Market-Products/MarketProducts.js
+++ eformative/src/pages/Market-Products/MarketProducts.js
@@ -15,7 +15,8 @@ const MarketProducts = () => {
       <h1 className='market-product-header'>View Products</h1>
       {product?.map((product, displayProduct) => (
         <div key={displayProduct}>
-          <a href='/item/:id'>
+          <img alt='product' src='http://placehold.jp/150x150.png'></img>
+          <a className='product-link' href='/item/:id'>
             {product.product}
             {"   "}
             {product.price}
