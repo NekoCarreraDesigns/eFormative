f6ed8104a00e4eb308b0f8422359a6de5240f007
NekoCarreraDesigns <denvermaas@gmail.com>
2022-12-27 16:00:03 -0700

    working on searchbar

--- eformative/src/pages/Market/Market.js
+++ eformative/src/pages/Market/Market.js
@@ -4,59 +4,50 @@ import "./Market.css";
 
 const Market = () => {
   const [items, setItems] = useState([]);
-
-  const search = () => {
-    const sellerFilterSearchInput =
-      document.getElementById("seller-search-bar");
-    const productFilterSearchInput =
-      document.getElementById("product-search-bar");
-    const priceFilterSearchInput = document.getElementById("price-search-bar");
-    const searchParams = {
-      sellerName: sellerFilterSearchInput.value,
-      product: productFilterSearchInput.value,
-      price: priceFilterSearchInput.value,
-    };
-
-    axios
-      .get("/market", { params: searchParams })
-      .then((res) => setItems(res.data))
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+  const [searchTerm, setSearchTerm] = useState("");
+  const [displayedItems, setDisplayedItems] = useState([]);
 
   useEffect(() => {
-    search();
-  });
+    const fetchItems = async () => {
+      const res = await axios.get("/market", {
+        params: { search: searchTerm },
+      });
+      setItems(res.data);
+    };
+    fetchItems();
+  }, [searchTerm]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const searchBarInput = event.target;
+    setSearchTerm(searchBarInput.value);
+    setDisplayedItems(
+      items.filter((item) => item.product.includes(searchTerm))
+    );
+  };
 
   return (
     <>
       <h1 className='market-page-header'>Market</h1>
       <div className='market-filter-div'>
         <div className='market-filter-input-div'>
-          <h2 className='market-search-filters-header'>Search filters</h2>
-          <input
-            className='market-filter-input'
-            type='text'
-            placeholder='search by seller'
-            id='seller-search-bar'></input>
-          <button onClick={search}>Seller Search</button>
-          <input
-            className='market-filter-input'
-            type='text'
-            placeholder='search by product'
-            id='product-search-bar'></input>
-          <button onClick={search}>Product Search</button>
-          <input
-            className='market-filter-input'
-            type='text'
-            placeholder='search by price'
-            id='price-search-bar'></input>
-          <button onClick={search}>Price Search</button>
+          <h2 className='market-search-filters-header'>Search Here</h2>
+          <form onSubmit={handleSearch}>
+            <input
+              className='market-filter-input'
+              type='text'
+              placeholder='search here'
+              id='search-bar'
+              value={searchTerm}></input>
+            <button type='submit'>Search</button>
+            <button type='button' onClick={() => setSearchTerm("")}>
+              Clear
+            </button>
+          </form>
         </div>
       </div>
-      {items.length > 0 ? (
-        items.map((item, sellerItem) => (
+      {displayedItems.length > 0 ? (
+        displayedItems.map((item, sellerItem) => (
           <div className='item-card' key={sellerItem}>
             <div className='item'>
               <h1 className='item-seller-header'>{item.sellerName}</h1>
@@ -68,7 +59,11 @@ const Market = () => {
           </div>
         ))
       ) : (
-        <p>No results found</p>
+        <div className='alert-div'>
+          <p className='alert-paragraph'>
+            No results found! Could be a misspelling or it is not there
+          </p>
+        </div>
       )}
     </>
   );
