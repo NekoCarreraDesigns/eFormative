ca25e01bc7a1d71441265ae0d5abdfc228185e9c
NekoCarreraDesigns <denvermaas@gmail.com>
2023-01-10 14:23:29 -0700

    changed connection to except session store

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,10 @@
 // dependencies and imports
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const { MongoClient } = require("mongodb");
 // variables
 const Db = process.env.ATLAS_URI;
+
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,6 +21,20 @@ module.exports = {
       }
       return callback(err);
     });
+  },
+  setUpSession: function (app) {
+    app.use(
+      session({
+        secret: "Wednesday's child is full of woe",
+        store: new MongoStore({
+          client: client,
+          db: _db,
+          stringify: false,
+        }),
+        resave: false,
+        saveUninitialized: true,
+      })
+    );
   },
 
   getDb: function () {
