b193e0978b6566fe20dc495aca94a773e272b2f8
NekoCarreraDesigns <denvermaas@gmail.com>
2023-01-19 15:57:57 -0700

    added code for material-ui and a save item function

--- eformative/src/pages/Market/Market.js
+++ eformative/src/pages/Market/Market.js
@@ -10,12 +10,18 @@ import {
   CardContent,
   CardActions,
   Button,
+  TextField,
+  IconButton,
 } from "@mui/material";
+
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const Market = () => {
   const [items, setItems] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [displayedItems, setDisplayedItems] = useState([]);
+  const [savedItems, setSavedItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -27,9 +33,24 @@ const Market = () => {
     fetchItems();
   }, [searchTerm]);
 
+  const handleSave = (itemId) => {
+    setLoading(true);
+    axios
+      .post("/items/saved", { itemId })
+      .then((response) => {
+        console.log(response);
+        setSavedItems([...savedItems, response.data]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
   const handleSearch = (event) => {
     event.preventDefault();
-    const searchBarInput = event.target;
+    const searchBarInput = event.target.value;
     setSearchTerm(searchBarInput.value);
     setDisplayedItems(
       items.filter((item) => item.product.includes(searchTerm))
@@ -38,47 +59,73 @@ const Market = () => {
 
   return (
     <>
-      <h1 className='market-page-header'>Market</h1>
+      <Typography variant='h3' className='market-page-header'>
+        Market
+      </Typography>
       <div className='market-filter-div'>
         <div className='market-filter-input-div'>
-          <h2 className='market-search-filters-header'>Search Here</h2>
           <form onSubmit={handleSearch}>
-            <input
+            <TextField
               className='market-filter-input'
               type='text'
               placeholder='search here'
               id='search-bar'
-              value={searchTerm}></input>
-            <button type='submit'>Search</button>
-            <button type='button' onClick={() => setSearchTerm("")}>
+              value={searchTerm}></TextField>
+            <Button
+              variant='contained'
+              color='success'
+              size='medium'
+              className='search-button'
+              type='submit'>
+              Search
+            </Button>
+            <Button
+              className='clear-button'
+              variant='contained'
+              size='medium'
+              type='button'
+              color='error'
+              onClick={() => setSearchTerm("")}>
               Clear
-            </button>
+            </Button>
           </form>
         </div>
       </div>
-      <Grid container spacing={3}>
-        {displayedItems.map((item, index) => (
-          <Grid item xs={3} key={index}>
-            <Card variant='outlined' elevation={6} className='item-card'>
-              <CardHeader title={item.product} subheader={item.sellerName} />
-              <CardMedia
-                image='http://placehold.jp/150x150.png'
-                title={item.product}
-              />
-              <CardContent>
-                <Typography variant='body2' color='textSecondary' component='p'>
-                  {item.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size='small' color='primary'>
-                  {item.price}
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      {displayedItems.length > 0 ? (
+        <Grid container spacing={3}>
+          {displayedItems.map((item, index) => (
+            <Grid item xs={3} key={index}>
+              <Card elevation={6} className='item-card'>
+                <CardHeader title={item.product} subheader={item.sellerName} />
+                <CardMedia
+                  image='http://placehold.jp/150x150.png'
+                  title={item.product}
+                />
+                <CardContent>
+                  <Typography
+                    variant='body2'
+                    color='textSecondary'
+                    component='p'>
+                    {item.description}
+                    {item.price}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <IconButton onClick={() => handleSave(item.id)}>
+                    {/* <FavoriteBorderIcon /> */}
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <div className='alert-div'>
+          <Typography variant='body1' className='alert-paragraph'>
+            No results found! Could be a misspelling or it is not here
+          </Typography>
+        </div>
+      )}
     </>
   );
 };
