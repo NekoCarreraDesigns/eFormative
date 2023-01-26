1e2be2424bd011ec2ae7ad77b6266338c623fbf5
NekoCarreraDesigns <denvermaas@gmail.com>
2023-01-26 15:56:00 -0700

    Fixed grid layout for photos

--- eformative/src/pages/Home/Home.js
+++ eformative/src/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const getLocation = () => {
@@ -12,64 +14,26 @@ const Home = () => {
   return (
     <>
       <div className='background-image'>
-        <h1 className='home-page-header'>
+        {/* <h1 className='home-page-header'>
           <img className='logo' alt='logo' src='./assets/eLogo.jpg'></img>
           formative
-        </h1>
+        </h1> */}
         <p className='home-page-paragraph'>Help yourself, help the planet</p>
         <div className='image-container'>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image001.jpg'></img>
-          </div>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image002.jpg'></img>
-          </div>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image003.jpg'></img>
-          </div>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image004.jpg'></img>
-          </div>
-        </div>
-        <div className='image-container'>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image005.jpg'></img>
-          </div>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image006.jpg'></img>
-          </div>
-          <div className='image-row'>
-            <img
-              className='image'
-              alt='image1'
-              src='./assets/image007.jpg'></img>
-          </div>
+          <img className='image' alt='image1' src='./assets/image001.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image002.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image003.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image004.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image005.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image006.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image007.jpg'></img>
+          <img className='image' alt='image1' src='./assets/image008.jpg'></img>
         </div>
         <div className='paragraph-container'>
           <p className='second-page-paragraph'>
-            <img
-              alt='electricity'
-              className='electric'
-              src='./assets/ElectricSymbol.jpg'></img>
-            Electric products, News, and Reviews
+            <FontAwesomeIcon icon={faBolt}></FontAwesomeIcon>
+            Electric products, News, and Reviews.
+            <br /> Have Fun, and help the planet!
           </p>
         </div>
         <div className='button-container'>
