c8ebeaa53f4cfb267e4d9192422e70c72a3a5e2b
NekoCarreraDesigns <denvermaas@gmail.com>
2023-01-27 16:27:01 -0700

    changed form to material-ui

--- eformative/src/pages/Post-Review/PostReview.js
+++ eformative/src/pages/Post-Review/PostReview.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import {
+  FormControl,
+  FormHelperText,
+  TextField,
+  Button,
+  TextareaAutosize,
+} from "@mui/material";
+import OnImageChange from "../../images/images";
 import "./PostReview.css";
 
 const addReview = () => {
@@ -34,12 +42,6 @@ const addReview = () => {
     });
 };
 
-// figure out to add eventlistener without being null, to prevent page refresh
-// form.addEventListener("click", (event) => {
-//   addReview();
-//   event.preventDefault();
-// });
-
 const PostReview = () => {
   const [input, setInput] = useState(" ");
   const inputHandler = (event) => {
@@ -50,38 +52,63 @@ const PostReview = () => {
     <>
       <h1 className='post-review-header'>Post A Review</h1>
       <form onSubmit={addReview} className='post-review-form'>
-        <input
-          className='seller-name-input'
-          id='seller-name'
-          type='text'
-          placeholder='please enter your name'></input>
-        <br />
-        <input
-          className='product-name-input'
-          id='product-name'
-          type='text'
-          placeholder='product being reviewed'></input>
-        <br />
-        <input
-          className='seller-review-input'
-          type='text'
-          placeholder='seller being reviewed'
-          id='seller-review'></input>
-        <button className='upload-button'>Upload Photo or video</button>
-        <br />
-        <textarea
-          maxLength={300}
-          className='user-post-review-textarea'
-          id='review-text-area'
-          onChange={inputHandler}></textarea>
-        <br />
-        <span className='character-count-span'>
-          <strong>{300 - input.length} characters left</strong>
-        </span>
-        <br />
-        <button className='post-review-button' type='submit'>
-          Submit
-        </button>
+        <FormControl>
+          <TextField
+            fullWidth
+            required
+            className='seller-name-input'
+            id='seller-name'
+            type='text'
+            placeholder='please enter your name'></TextField>
+          <br />
+          <br />
+          <TextField
+            fullWidth
+            className='product-name-input'
+            id='product-name'
+            type='text'
+            placeholder='product being reviewed'></TextField>
+          <br />
+          <br />
+          <TextField
+            fullWidth
+            className='seller-review-input'
+            type='text'
+            placeholder='seller being reviewed'
+            id='seller-review'></TextField>
+          <br />
+          <br />
+          <FormHelperText>Upload a picture or video</FormHelperText>
+          <input
+            type='file'
+            multiple
+            accept='image/*'
+            id='image-input'
+            name='post-item-picture-video'
+            placeholder='upload an image or a video'
+            onChange={OnImageChange}
+            className='upload-button'></input>
+          <br />
+          <br />
+          <TextareaAutosize
+            maxLength={300}
+            className='user-post-review-textarea'
+            id='review-text-area'
+            onChange={inputHandler}
+            style={{ width: "800px", height: "400px" }}></TextareaAutosize>
+          <br />
+          <span className='character-count-span'>
+            <strong>{300 - input.length} characters left</strong>
+          </span>
+          <br />
+          <Button
+            variant='contained'
+            color='success'
+            className='post-review-button'
+            type='submit'>
+            Submit
+          </Button>
+        </FormControl>
       </form>
     </>
   );
