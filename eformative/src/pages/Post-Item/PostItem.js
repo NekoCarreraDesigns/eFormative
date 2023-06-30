7869ee0f20b00123e2ac51cba209e900762965a8
NekoCarreraDesigns <denvermaas@gmail.com>
2023-06-30 15:11:01 -0600

    styled post-item page, fixed a spelling error, and styled the inputs and button

--- eformative/src/pages/Post-Item/PostItem.js
+++ eformative/src/pages/Post-Item/PostItem.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import {
-  FormControl,
-  Button,
-  TextareaAutosize,
-  TextField,
-  FormHelperText,
-} from "@mui/material";
+import { FormControl, TextareaAutosize, FormHelperText } from "@mui/material";
 import OnImageChange from "../../images/images";
 import "./PostItem.css";
 
@@ -44,77 +38,74 @@ const PostItem = () => {
 
   return (
     <>
-      <p className='post-item-paragraph'>Please post your item!</p>;
-      <div className='post-item-form-div'>
-        <form>
-          <FormControl>
-            <TextField
-              fullWidth
-              required
-              label='Please put your name'
-              variant='outlined'
-              className='seller-name-post-input'
-              id='seller-input'
-            />
-            <br />
-            <br />
+      <div className='hero-section'>
+        <p className='post-item-paragraph'>Please post your item!</p>
+        <div className='post-item-form-div'>
+          <form>
             <FormControl>
-              <TextField
-                fullWidth
-                required
-                label='Please input item name'
-                variant='outlined'
-                className='item-name-input'
-                id='item-input'
-              />
-              <br />
-              <br />
-              <FormHelperText>Upload a picture or video</FormHelperText>
               <input
-                type='file'
-                multiple
-                accept='image/*'
-                className='upload-picture-video-input'
-                id='image-input'
-                name='post-item-picture-video'
-                placeholder='upload an image or a video'
-                onChange={OnImageChange}
+                required
+                placeholder='What is your name'
+                variant='outlined'
+                className='seller-name-post-input text-input-white'
+                id='seller-input'
               />
+              <br />
+              <br />
+              <FormControl>
+                <input
+                  required
+                  placeholder='Input item name'
+                  variant='outlined'
+                  className='item-name-input text-input-white'
+                  id='item-input'
+                />
+                <br />
+                <br />
+                <FormHelperText>Upload a picture or video</FormHelperText>
+                <input
+                  type='file'
+                  multiple
+                  accept='image/*'
+                  className='upload-picture-video-input'
+                  id='image-input'
+                  name='post-item-picture-video'
+                  placeholder='upload an image or a video'
+                  onChange={OnImageChange}
+                />
+              </FormControl>
+
+              <input
+                required
+                placeholder='Please input a price'
+                variant='outlined'
+                className='item-price-input text-input-white'
+                id='price-input'
+              />
+              <br />
+              <br />
+              <TextareaAutosize
+                className='post-item-textarea'
+                onChange={characterCounter}
+                id='text-area'
+                placeholder='Enter item description'
+                maxLength={300}
+                style={{ width: "500px", height: "500px" }}
+              />
+
+              <br />
+              <span className='character-count-post-span'>
+                <strong>{300 - inputArea.length} characters left</strong>
+              </span>
+              <br />
+              <button
+                className='add-item-button clear-btn-green-border'
+                onClick={addItem}>
+                Post Item
+              </button>
             </FormControl>
-
-            <TextField
-              required
-              fullWidth
-              label='Please input a price'
-              variant='outlined'
-              className='item-price-input'
-              id='price-input'
-            />
-            <br />
-            <br />
-            <TextareaAutosize
-              className='post-item-textarea'
-              onChange={characterCounter}
-              id='text-area'
-              placeholder='Enter item description'
-              maxLength={300}
-              style={{ width: "500px", height: "500px" }}
-            />
-
-            <br />
-            <span className='character-count-post-span'>
-              <strong>{300 - inputArea.length} characters left</strong>
-            </span>
-            <br />
-            <Button
-              variant='contained'
-              color='success'
-              className='add-item-button'
-              onClick={addItem}>
-              Post Item
-            </Button>
-          </FormControl>
-        </form>
+          </form>
+        </div>
       </div>
     </>
   );
