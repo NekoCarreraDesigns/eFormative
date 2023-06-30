41cb53fd571f27cf0ca55fda8e8524d284a3a012
NekoCarreraDesigns <denvermaas@gmail.com>
2023-06-30 15:31:25 -0600

    styling the seller page

--- eformative/src/pages/Seller/Seller.js
+++ eformative/src/pages/Seller/Seller.js
@@ -39,33 +39,35 @@ const Seller = (props) => {
 
   return (
     <>
-      <div>
-        {user && (
-          <h1 className='seller-page-header'>Welcome, {user.firstName}!</h1>
-        )}
-      </div>
-      <button className='post-item-button' onClick={postItemRedirect}>
-        Post an item to sell
-      </button>
-      <button className='logout-button' onClick={handleLogout}>
-        Logout
-      </button>
+      <div className='hero-section'>
+        <div>
+          {user && (
+            <h1 className='seller-page-header'>Welcome {user.firstName}!</h1>
+          )}
+        </div>
+        <button className='post-item-button' onClick={postItemRedirect}>
+          Post an item to sell
+        </button>
+        <button className='logout-button' onClick={handleLogout}>
+          Logout
+        </button>
 
-      <div className='items-sold-div'>
-        <h1 className='items-sold-header'>Items Sold</h1>
-        <img alt='item' src='http://placehold.jp/150x150.png'></img>
+        <div className='items-sold-div'>
+          <h1 className='items-sold-header'>Items Sold</h1>
+          <img alt='item' src='http://placehold.jp/150x150.png'></img>
+        </div>
+        {items &&
+          items.map((item, displayItem) => (
+            <div key={displayItem} className='items-selling-div'>
+              <h1 className='items-selling-header'>Items Selling</h1>
+              <img alt='sale-item' src='http://placehold.jp/150x150.png'>
+                {item.image}
+              </img>
+              <h3>{item.product}</h3>
+              <h3>{item.price}</h3>
+            </div>
+          ))}
       </div>
-      {items &&
-        items.map((item, displayItem) => (
-          <div key={displayItem} className='items-selling-div'>
-            <h1 className='items-selling-header'>Items Selling</h1>
-            <img alt='sale-item' src='http://placehold.jp/150x150.png'>
-              {item.image}
-            </img>
-            <h3>{item.product}</h3>
-            <h3>{item.price}</h3>
-          </div>
-        ))}
     </>
   );
 };
