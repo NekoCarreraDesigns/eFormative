d8d9a85741231d47d0adf3bd4b4a040aab28793a
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-02 14:39:02 -0600

    removed unused variables

--- eformative/src/pages/Home/Home.js
+++ eformative/src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const getLocation = () => {
@@ -13,12 +11,16 @@ const Home = () => {
   };
 
   return (
-    <div className="body">
-      <div className="home-hero">
-        <img className="hero-logo" alt="logo" src="./assets/eformative-logo-white.png" />
-        <h2 className="home-page-paragraph">Have Fun, and help the planet!</h2>
-        <div className="button-container">
-          <button className="clear-btn-green-border" onClick={getLocation}>
+    <div className='body'>
+      <div className='home-hero'>
+        <img
+          className='hero-logo'
+          alt='logo'
+          src='./assets/eformative-logo-white.png'
+        />
+        <h2 className='home-page-paragraph'>Have Fun, and help the planet!</h2>
+        <div className='button-container'>
+          <button className='clear-btn-green-border' onClick={getLocation}>
             Find Your Market
           </button>
         </div>
@@ -30,17 +32,25 @@ const Home = () => {
 };
 
 const IconsSection = () => (
-  <div className="icons-section">
-    <div className="container">
-      <div className="row">
+  <div className='icons-section'>
+    <div className='container'>
+      <div className='row'>
         {[
-          { src: "./assets/electric-products-white.png", alt: "Electric Products Icon", title: "Electric Products" },
+          {
+            src: "./assets/electric-products-white.png",
+            alt: "Electric Products Icon",
+            title: "Electric Products",
+          },
           { src: "./assets/news-white.png", alt: "News Icon", title: "News" },
-          { src: "./assets/reviews-white.png", alt: "Reviews Icon", title: "Reviews" },
+          {
+            src: "./assets/reviews-white.png",
+            alt: "Reviews Icon",
+            title: "Reviews",
+          },
         ].map(({ src, alt, title }) => (
-          <div className="icons-column shake-on-hover" key={title}>
-            <img className="icons-column-image" src={src} alt={alt} />
-            <h2 className="icons-column-title">{title}</h2>
+          <div className='icons-column shake-on-hover' key={title}>
+            <img className='icons-column-image' src={src} alt={alt} />
+            <h2 className='icons-column-title'>{title}</h2>
           </div>
         ))}
       </div>
@@ -57,18 +67,25 @@ const SignupCTASection = () => {
   };
 
   return (
-    <div className="signup-cta-section">
-      <div className="container">
-        <div className="columns">
-          <div className="left-column">
-            <img className="signup-cta-image" alt="Couple on eScooter" src="./assets/emotoscooter-cut.png" />
+    <div className='signup-cta-section'>
+      <div className='container'>
+        <div className='columns'>
+          <div className='left-column'>
+            <img
+              className='signup-cta-image'
+              alt='Couple on eScooter'
+              src='./assets/emotoscooter-cut.png'
+            />
           </div>
-          <div className="right-column">
-            <h2 className="cta-header">Join the Green Marketplace</h2>
-            <p className="cta-body">
-              Contribute to a sustainable future by buying and selling electric products with ease.
+          <div className='right-column'>
+            <h2 className='cta-header'>Join the Green Marketplace</h2>
+            <p className='cta-body'>
+              Contribute to a sustainable future by buying and selling electric
+              products with ease.
             </p>
-            <button className="signup-cta-button" onClick={signUpRedirect}>Sign Up Now</button>
+            <button className='signup-cta-button' onClick={signUpRedirect}>
+              Sign Up Now
+            </button>
           </div>
         </div>
       </div>
