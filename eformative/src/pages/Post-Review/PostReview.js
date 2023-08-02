94b8484ea7f3fc9e2b1246a0d658ac739557ea98
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-02 14:40:09 -0600

    removed unused variable

--- eformative/src/pages/Post-Review/PostReview.js
+++ eformative/src/pages/Post-Review/PostReview.js
@@ -5,7 +5,6 @@ import {
   FormControl,
   FormHelperText,
   TextField,
-  Button,
   TextareaAutosize,
 } from "@mui/material";
 import OnImageChange from "../../images/images";
@@ -50,69 +49,66 @@ const PostReview = () => {
 
   return (
     <>
-      <div className="hero-section">
-        <h1 className="post-review-header">Post A Review</h1>
+      <div className='hero-section'>
+        <h1 className='post-review-header'>Post A Review</h1>
       </div>
-      <div className="review-container">
-        <form onSubmit={addReview} className="post-review-form">
-          <div className="form-control-wrapper">
-            <FormControl className="custom-form-control">
+      <div className='review-container'>
+        <form onSubmit={addReview} className='post-review-form'>
+          <div className='form-control-wrapper'>
+            <FormControl className='custom-form-control'>
               <TextField
                 fullWidth
                 required
-                className="seller-name-input text-input-dark"
-                id="seller-name"
-                type="text"
-                placeholder="Please enter your name"
+                className='seller-name-input text-input-dark'
+                id='seller-name'
+                type='text'
+                placeholder='Please enter your name'
               />
               <br />
               <br />
               <TextField
                 fullWidth
-                className="product-name-input text-input-dark"
-                id="product-name"
-                type="text"
-                placeholder="Product being reviewed"
+                className='product-name-input text-input-dark'
+                id='product-name'
+                type='text'
+                placeholder='Product being reviewed'
               />
               <br />
               <br />
               <TextField
                 fullWidth
-                className="seller-review-input text-input-dark"
-                type="text"
-                placeholder="Seller being reviewed"
-                id="seller-review"
+                className='seller-review-input text-input-dark'
+                type='text'
+                placeholder='Seller being reviewed'
+                id='seller-review'
               />
               <br />
               <br />
               <FormHelperText>Upload a picture or video</FormHelperText>
               <input
-                type="file"
+                type='file'
                 multiple
-                accept="image/*"
-                id="image-input"
-                name="post-item-picture-video"
-                placeholder="Upload an image or a video"
+                accept='image/*'
+                id='image-input'
+                name='post-item-picture-video'
+                placeholder='Upload an image or a video'
                 onChange={OnImageChange}
-                className="upload-button"
+                className='upload-button'
               />
               <br />
               <br />
               <TextareaAutosize
                 maxLength={300}
-                className="user-post-review-textarea text-input-dark"
-                id="review-text-area"
+                className='user-post-review-textarea text-input-dark'
+                id='review-text-area'
                 onChange={inputHandler}
               />
               <br />
-              <span className="character-count-span">
+              <span className='character-count-span'>
                 <strong>{300 - input.length} characters left</strong>
               </span>
               <br />
-              <button
-                className="post-review-button clear-btn-sm"
-                type="submit"
-              >
+              <button className='post-review-button clear-btn-sm' type='submit'>
                 Submit
               </button>
             </FormControl>
