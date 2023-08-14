eb9b0a555597a6f9d8cb7c8e25c7e8cba62863db
NekoCarreraDesigns <denvermaas@gmail.com>
2023-08-14 13:24:28 -0600

    removed pagoantion code

--- eformative/src/pages/Reviews/Reviews.js
+++ eformative/src/pages/Reviews/Reviews.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Pagination } from "@mui/material";
 import axios from "axios";
 import "./Reviews.css";
 
@@ -48,45 +47,45 @@ const Reviews = () => {
       <div className='hero-section'>
         <h1 className='reviews-page-header'>Reviews</h1>
         <div className='search-filters-div'>
-        <div className='seller-filter-div'>
-          <div className='search-row'>
-            <div className='search-input-button'>
-              <input
-                id='seller-search'
-                className='text-input-white'
-                type='text'
-                placeholder='Search by seller name'
-                input={input}
-                onChange={inputHandler}
-              />
-              <button
-                className='clear-btn-sm search seller-search-button'
-                onClick={sellerFilter}
-              >
-                Search Seller
-              </button>
+          <div className='seller-filter-div'>
+            <div className='search-row'>
+              <div className='search-input-button'>
+                <input
+                  id='seller-search'
+                  className='text-input-white'
+                  type='text'
+                  placeholder='Search by seller name'
+                  input={input}
+                  onChange={inputHandler}
+                />
+                <button
+                  className='clear-btn-sm search seller-search-button'
+                  onClick={sellerFilter}>
+                  Search Seller
+                </button>
+              </div>
+              <div className='search-input-button'>
+                <input
+                  id='product-search'
+                  className='text-input-white'
+                  type='text'
+                  placeholder='Search by product name'
+                  input={input}
+                  onChange={inputHandler}
+                />
+                <button
+                  className='clear-btn-sm product-search-button'
+                  onClick={productFilter}>
+                  Search Product
+                </button>
+              </div>
             </div>
-            <div className='search-input-button'>
-              <input
-                id='product-search'
-                className='text-input-white'
-                type='text'
-                placeholder='Search by product name'
-                input={input}
-                onChange={inputHandler}
-              />
-              <button
-                className='clear-btn-sm product-search-button'
-                onClick={productFilter}
-              >
-                Search Product
-              </button>
-            </div>
+            <button
+              className='clear-btn-green-border post-a-review-button'
+              onClick={postReview}>
+              Post A Review
+            </button>
           </div>
-          <button className='clear-btn-green-border post-a-review-button' onClick={postReview}>
-            Post A Review
-          </button>
-        </div>
         </div>
       </div>
       <div className='reviews-container'>
@@ -100,14 +99,14 @@ const Reviews = () => {
           </div>
         ))}
       </div>
-      <div className="page-pagination-container">
+      {/* <div className="page-pagination-container">
         <Pagination
           className='page-pagination'
           color='primary'
           variant='outlined'
           count={20}>
         </Pagination>
-      </div>
+      </div> */}
     </>
   );
 };
