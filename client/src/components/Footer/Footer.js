ad9253e171ee317fa2dcaf70bb4d2e249b6c0ec5
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-11 15:15:03 -0600

    removed mine and mark's names from the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,14 +4,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className='page-footer'>
-      {/* <p className='footer-paragraph'>
-        Thanks for visiting this website, we appreciate it. do you have a
-        question or are you trying to report a problem with the site? Please
-        email nekocarreradesigns@gmail.com
-      </p> */}
-      <small className='footer-small-print'>
-        &copy; 2023 eformative  |  Website by <a href="mailto:nekocarreradesigns@gmail.com">Neko Carrera Designs</a> & <a href="https://hagercreative.com">HagerCreative</a>
-      </small>
+      <small className='footer-small-print'>&copy; 2023 eformative</small>
+      <br />
+      <a href='/about'>about us</a>
     </footer>
   );
 };
