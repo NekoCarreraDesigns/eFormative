44a7294146de4f9748b93a71fc318fe97a4492fe
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-12 15:11:25 -0600

    added and styeld about page

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1 className='about-page-header'>About Us</h1>
+        <p className='about-page-paragraph'>
+          Eformative site created by Neko Carrera Designs, graphics by Hager
+          Creative, site concept by Scott Harrah.
+          <br />
+          The site was created to start an alternative marketplace for electric
+          transportation alternatives to create a greener environment for all of
+          us to live in. It is our belief that we can make the world a better
+          place and help stave off future destruction of the environment by
+          joining together to save it.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
