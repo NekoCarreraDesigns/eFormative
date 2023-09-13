83af768b078466fbc808a718018692e81339bb4a
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-13 15:46:09 -0600

    added node modules to gitignore

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // configuration for dotenv
 require("dotenv").config({ path: "./config.env" });
 // declaring the port to be used for the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 // express middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
