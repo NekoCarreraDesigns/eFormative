dda6001cca634b57e01bc47941d8a1455b76c4cd
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-15 14:50:03 -0600

    added files with code for admin registration

---
+++ client/src/pages/Admin/adminRegistration.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Admin.css";
+import axios from "axios";
+
+const AdminRegistration = () => {
+  const navigate = useNavigate();
+  const [pin, setPin] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handlePinChange = (event) => {
+    setPin(event.target.value);
+  };
+
+  const handleRegistration = () => {
+    let path = `/admin-login`;
+    axios
+      .post("/admin/register-pin", { pin })
+      .then((res) => {
+        if (res.data.success) {
+          setMessage("PIN registered successfully");
+          navigate(path);
+        } else {
+          setMessage("PIN was not registered");
+        }
+      })
+      .catch((error) => {
+        setMessage("An error occurred while trying yo register PIN");
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h2>Admin Registration</h2>
+      <div>
+        <label>Enter Admin PIN:</label>
+        <input type='text' value={pin} onChange={handlePinChange} />
+      </div>
+      <div>
+        <button onClick={handleRegistration}>Register</button>
+      </div>
+      <div>{message}</div>
+    </div>
+  );
+};
+export default AdminRegistration;
