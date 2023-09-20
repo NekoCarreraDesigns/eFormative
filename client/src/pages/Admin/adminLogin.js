815562b1a332dc2de9afd5cc62ec08886324ba04
NekoCarreraDesigns <denvermaas@gmail.com>
2023-09-20 16:18:23 -0600

    added logic to admin login page

---
+++ client/src/pages/Admin/adminLogin.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Admin.css";
+
+const AdminLogin = () => {
+  let path = `/admin`;
+  let navigate = useNavigate();
+  const [pin, setPin] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handlePinChange = (event) => {
+    setPin(event.target.value);
+  };
+
+  const handleLogin = () => {
+    axios
+      .post("/admin/check-pin", { pin })
+      .then((response) => {
+        if (response.data.success) {
+          setMessage("login successful");
+          navigate(path);
+        } else {
+          setMessage("PIN is incorrect");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage("An error has occurred");
+      });
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+      <div>
+        <label>Enter Admin PIN:</label>
+        <input type='text' value={pin} onChange={handlePinChange} />
+      </div>
+      <div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      <div>{message}</div>
+    </div>
+  );
+};
+
+export default AdminLogin;
