914e5bbdc7b421f33a2d395df7d0cdad25406d65
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-12 14:16:00 -0600

    removed unnecessary packages

--- server/server.js
+++ server/server.js
@@ -27,8 +27,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(require("./routes/page"));
 // code for react serverless functioning
-app.use(express.static(path.join(__dirname, "client/build")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
