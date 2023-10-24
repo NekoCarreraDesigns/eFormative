24ab51ca92fbf299832e05f967ac206b9c5331ec
NekoCarreraDesigns <denvermaas@gmail.com>
2023-10-24 15:56:06 -0600

    removed serachbars and added Searchbar component to reviews page

--- client/src/pages/Reviews/Reviews.js
+++ client/src/pages/Reviews/Reviews.js
@@ -1,39 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import "./Reviews.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Reviews = () => {
-  const [input, setInput] = useState("");
   const [reviews, setReview] = useState();
   let navigate = useNavigate();
 
   const postReview = () => {
     let postPath = `/post-reviews`;
     navigate(postPath);
-  };
-
-  const inputHandler = (event) => {
-    let lowerCase = event.target.value.toLowerCase();
-    setInput(lowerCase);
-  };
-
-  const sellerFilter = () => {
-    const sellerSearchInput = document.getElementById("seller-search");
-    axios.get("/seller/reviews", {
-      sellerReview: sellerSearchInput.value,
-    });
-    navigate("/seller-reviews");
-    console.log("clicked");
-  };
-
-  const productFilter = async () => {
-    const productSearchInput = document.getElementById("product-search");
-    await axios.get("/reviews", { productName: productSearchInput.value });
-    navigate("/product-reviews");
-    console.log("clicked");
-    return productFilter;
   };
 
   useEffect(() => {
@@ -46,47 +23,12 @@ const Reviews = () => {
     <>
       <div className='hero-section'>
         <h1 className='reviews-page-header'>Reviews</h1>
-        <div className='search-filters-div'>
-          <div className='seller-filter-div'>
-            <div className='search-row'>
-              <div className='search-input-button'>
-                <input
-                  id='seller-search'
-                  className='text-input-white'
-                  type='text'
-                  placeholder='Search by seller name'
-                  input={input}
-                  onChange={inputHandler}
-                />
-                <button
-                  className='clear-btn-sm search seller-search-button'
-                  onClick={sellerFilter}>
-                  Search Seller
-                </button>
-              </div>
-              <div className='search-input-button'>
-                <input
-                  id='product-search'
-                  className='text-input-white'
-                  type='text'
-                  placeholder='Search by product name'
-                  input={input}
-                  onChange={inputHandler}
-                />
-                <button
-                  className='clear-btn-sm product-search-button'
-                  onClick={productFilter}>
-                  Search Product
-                </button>
-              </div>
-            </div>
+       <SearchBar/>
             <button
               className='clear-btn-green-border post-a-review-button'
               onClick={postReview}>
               Post A Review
             </button>
-          </div>
-        </div>
       </div>
       <div className='reviews-container'>
         {reviews?.map((review, userReview) => (
