17dbdfab8fe4a8173268ce8fec5afeeb2e252f98
NekoCarreraDesigns <denvermaas@gmail.com>
2024-01-23 17:20:14 -0700

    added aria-labels for accessibility

--- client/src/pages/Post-Review/PostReview.js
+++ client/src/pages/Post-Review/PostReview.js
@@ -49,10 +49,9 @@ const PostReview = () => {
 
   return (
     <>
-      <div className='hero-section'>
-        <h1 className='post-review-header'>Post A Review</h1>
-      </div>
-      <div className='review-container'>
+      
+      <div className='review-container hero-section'>
+        <h1 className='post-review-header'>Post a review!</h1>
         <form onSubmit={addReview} className='post-review-form'>
           <div className='form-control-wrapper'>
             <FormControl className='custom-form-control'>
@@ -62,6 +61,7 @@ const PostReview = () => {
                 className='seller-name-input text-input-dark'
                 id='seller-name'
                 type='text'
+                aria-label="Name input for the reviewer"
                 placeholder='Please enter your name'
               />
               <br />
@@ -72,6 +72,7 @@ const PostReview = () => {
                 id='product-name'
                 type='text'
                 placeholder='Product being reviewed'
+                aria-label="name of the product being reviewed input"
               />
               <br />
               <br />
@@ -81,6 +82,7 @@ const PostReview = () => {
                 type='text'
                 placeholder='Seller being reviewed'
                 id='seller-review'
+                aria-label=" name of seller being reviewed"
               />
               <br />
               <br />
@@ -91,6 +93,7 @@ const PostReview = () => {
                 accept='image/*'
                 id='image-input'
                 name='post-item-picture-video'
+                aria-label="user image upload button"
                 placeholder='Upload an image or a video'
                 onChange={OnImageChange}
                 className='upload-button'
@@ -101,6 +104,7 @@ const PostReview = () => {
                 maxLength={300}
                 className='user-post-review-textarea text-input-dark'
                 id='review-text-area'
+                aria-label="user textarea to write reviews about sellers or products"
                 onChange={inputHandler}
               />
               <br />
@@ -108,7 +112,7 @@ const PostReview = () => {
                 <strong>{300 - input.length} characters left</strong>
               </span>
               <br />
-              <button className='post-review-button clear-btn-sm' type='submit'>
+              <button className='clear-btn-green-border clear-btn-sm' type='submit' aria-label="click button to submit your review">
                 Submit
               </button>
             </FormControl>
