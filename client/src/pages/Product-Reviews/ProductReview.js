2194a48709bec1829627d19746f0f0fe4dcbe0a6
NekoCarreraDesigns <denvermaas@gmail.com>
2024-01-23 17:39:15 -0700

    added aria label and fixed page

--- client/src/pages/Product-Reviews/ProductReview.js
+++ client/src/pages/Product-Reviews/ProductReview.js
@@ -10,13 +10,11 @@ const ProductReview = () => {
   });
   return (
     <>
-      <div className='hero-section'>
-        <h1 className='product-review-header'>Product Reviews</h1>
-      </div>
-      <div className='reviews-container'>
+      <div className='reviews-container hero-section'>
+      <h1 className='product-review-header'>Product Reviews</h1>
         {products?.map((products, productDisplay) => (
           <div key={productDisplay} className='reviews-item'>
-            <h1 className='reviews-header'>
+            <h1 className='reviews-header' aria-label='name of the product and reviewer'>
               Product:{products.productName}, Reviewer:{products.reviewerName}
             </h1>{" "}
             <div className='reviews-div'>{products.review}</div> <hr />
