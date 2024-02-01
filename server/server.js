6d4ec22ce3d98e4133334c8490a091244d621e56
NekoCarreraDesigns <denvermaas@gmail.com>
2024-01-31 17:06:42 -0700

    added code to server for authentication

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport'); 
 // configuration for dotenv
 require("dotenv").config({ path: "./config.env" });
 // declaring the port to be used for the server
@@ -22,23 +25,40 @@ const PORT = process.env.PORT || 3001;
 // };
 // express middleware
 // app.use(cors(corsOptions));
+
+// passport setup for authentication
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {secure: false}
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash())
+
+// middleware for routes
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes/page"));
+
 // code for react serverless functioning
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
 // connection for the db
 const db = require("./db/connection");
+
 // function to make the server listen
 app.listen(PORT, () => {
-  db.connectToServer(function (err, req, res, next) {
+  db.connectToServer(function (err) {
     if (err) {
-      console.error(err);
+      console.error("App was unable to connect to database",err);
       process.exit(1);
     } else {
       db.setUpSession(app);
